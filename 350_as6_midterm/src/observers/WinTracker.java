5ce409af9cef5dafa63224810fd5eb3388eb2b7d
Jordan <jordanaensing@gmail.com>
2022-04-28 15:32:19 -0400

    Basic WinTracker functionality

--- 350_as6_midterm/src/observers/WinTracker.java
+++ 350_as6_midterm/src/observers/WinTracker.java
@@ -1,12 +1,35 @@
 package observers;
 
+
 import interfaces.Subscriber;
 import objects.Team;
 
 public class WinTracker implements Subscriber{
-
+	
+	private int matchNumber = 0;
+	private int[][] matchScores;
+	private String[][] matchTeams;
+	
 	@Override
 	public void update(Team team1, Team team2, boolean gameOver) {
+		
+		if (gameOver) {
+			matchScores[matchNumber][0] = team1.getScore();
+			matchScores[matchNumber][1] = team2.getScore();
+			matchTeams[matchNumber][0] = team1.getName();
+			matchTeams[matchNumber][1] = team2.getName();
+			matchNumber++;
+		}
+		
+	}
+	
+	public String getMatches() {
+		
+		String matchTable = "";
+		for (int i = 0; i <= matchNumber; i++) {
+			matchTable += matchTeams[i][0] + " " + matchScores[i][0] + " vs. " + matchScores[i][1] + " " + matchTeams[i][1];
+		}
+		return matchTable;
 		
 	}
 	
